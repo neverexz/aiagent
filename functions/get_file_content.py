616b7815f5aad7d411151bdc6dc4da21a7a0fc95
neverexz <cool-eg@mail.ru>
2025-09-26 16:54:22 +0300

    first

---
+++ functions/get_file_content.py
@@ -0,0 +1,4 @@
+
+
+def get_file_content(working_directory, file_path):
+    pass
---
+++ main.py
@@ -0,0 +1,47 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
+import sys
+from functions.get_files_info import get_files_info
+
+
+def main():
+    
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+
+    if len(sys.argv) < 2:
+        print("no prompt")
+        return 
+    
+    prompt = sys.argv[1]
+        
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=prompt)])
+    ]
+    
+    response = client.models.generate_content(
+        model='gemini-2.0-flash-001',
+        contents=messages
+    )
+    
+    if response is None or response.usage_metadata is None:
+        print("ERROR")
+    
+    else:
+        if sys.argv[3] == '--verbose':
+            
+            print(f'User prompt:{response.text}')
+            
+            print(f'Prompt tokens: {response.usage_metadata.prompt_token_count}')
+            print(f'Response tokens: {response.usage_metadata.candidates_token_count}')
+            
+        else:
+            print(response.text)
+        
+        
+if __name__ == "__main__":
+    main()
+    # print(get_files_info("calculator", 'pkg'))
